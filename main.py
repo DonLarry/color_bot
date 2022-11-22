1bf7a84b2bc8e36b4adee45ff44c3f937e35d051
DonLarry <donlarry.timoteo@gmail.com>
2022-11-21 20:08:07 -0400

    Using local_settings.py

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import os
-
 from discord_bot import discord_bot
 
-from settings import DEBUG
+from settings import DISCORD_BOT_TOKEN
 
 
 if __name__ == '__main__':
     bot = discord_bot()
-    env_identifier = 'DISCORD_BOT_TOKEN_TEST' if DEBUG else 'DISCORD_BOT_TOKEN'
-    # env_identifier = 'DISCORD_BOT_TOKEN'
-    bot.run(os.getenv(env_identifier))
+    bot.run(DISCORD_BOT_TOKEN)
